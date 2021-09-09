f3a790a81a7e7ed763a8fd28847db6e6f81469cb
zhuangzhihong <1149869346@qq.com>
2021-09-09 22:24:35 +0800

    发送邮件功能，邮件HTML模板

---
+++ src/test/java/com/zzkk/community/TestTextMail.java
@@ -0,0 +1,38 @@
+package com.zzkk.community;
+
+import com.zzkk.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zzkk
+ * @ClassName TestTextMail
+ * @Description Todo
+ **/
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TestTextMail {
+    @Resource
+    private MailClient mailClient;
+
+    @Resource
+    private  TemplateEngine templateEngine;
+
+    @Test
+    public void testMail(){
+        mailClient.sendMail("zhuangshao0826@163.com","offer","恭喜你被录用！");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","zzkk");
+        String content = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail("zhuangshao0826@163.com","offer",content);
+    }
+}
